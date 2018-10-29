76be440ed5a7db913b722cbd3ba12ed0aa5e55ef
ArseniyKolosha <35842248+ArseniyKolosha@users.noreply.github.com>
2018-10-29 17:44:02 +0200

    Initial Commit

---
+++ 5buttonsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  5buttonsTest
+//
+//  Created by Ars on 10/29/18.
+//  Copyright © 2018 ArsenIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
